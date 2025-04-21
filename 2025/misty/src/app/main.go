36f662426746bce220c5f0b2b3ad498df5e5133e
Mouhib Chamsi <94466413+chxmxii@users.noreply.github.com>
2025-04-21 21:16:40 +0000

    feat(misty): add challenge source code

---
+++ 2025/misty/src/app/main.go
@@ -0,0 +1,64 @@
+package main
+
+import (
+	"context"
+	"log"
+
+	"github.com/minio/minio-go/v7"
+	"github.com/minio/minio-go/v7/pkg/credentials"
+)
+
+var (
+	Cfg = struct {
+		EndPoint        string
+		AccessKeyID     string
+		SecretAccessKey string
+		UseSSL          bool
+	}{
+		EndPoint:        "localhost:9000",
+		AccessKeyID:     "79b64067dba07c88ed83af7b5e",
+		SecretAccessKey: "giClfHTLrRPF2XkMc0F6w7zL929JbFp",
+		UseSSL:          false,
+	}
+
+	BucketName = "test-mist37"
+	Location   = "us-west-1"
+)
+
+func main() {
+	ctx := context.Background()
+
+	// init minio client
+	minioClient, err := minio.New(Cfg.EndPoint, &minio.Options{
+		Creds:  credentials.NewStaticV4(Cfg.AccessKeyID, Cfg.SecretAccessKey, ""),
+		Secure: Cfg.UseSSL,
+	})
+	if err != nil {
+		log.Fatalln("Failed to initialize MinIO client:", err)
+	}
+
+	// create bucket
+	err = minioClient.MakeBucket(ctx, BucketName, minio.MakeBucketOptions{Region: Location})
+	if err != nil {
+		exists, errBucketExists := minioClient.BucketExists(ctx, BucketName)
+		if errBucketExists == nil && exists {
+			log.Printf("We already own bucket %s\n", BucketName)
+		} else {
+			log.Fatalln("Failed to create bucket:", err)
+		}
+	} else {
+		log.Printf("Successfully created bucket %s\n", BucketName)
+	}
+
+	// upload the file
+	objectName := "data-sample37"
+	filePath := "./flag.txt"
+	contentType := "application/octet-stream"
+
+	info, err := minioClient.FPutObject(ctx, BucketName, objectName, filePath, minio.PutObjectOptions{ContentType: contentType})
+	if err != nil {
+		log.Fatalln("Failed to upload file:", err)
+	}
+
+	log.Printf("Successfully uploaded %s of size %d bytes\n", objectName, info.Size)
+}
