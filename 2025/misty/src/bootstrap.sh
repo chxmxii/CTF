d23c9fd853eb0ab55aa17b920671ccd9b3cada36
Mouhib Chamsi <94466413+chxmxii@users.noreply.github.com>
2025-04-22 16:59:43 +0000

    feat(misty): add bootstrap script for misty chall

--- 2025/misty/src/bootstrap.sh
+++ 2025/misty/src/bootstrap.sh
@@ -2,17 +2,17 @@
 
 set -euo pipefail
 
-# Admin credentials (bootstrap user)
+# admin creds
 ADMIN_ENDPOINT="localhost:9000"
 ADMIN_ACCESS_KEY="admin-access-key"
 ADMIN_SECRET_KEY="admin-secret-key"
 ADMIN_ALIAS="admin"
 
 # User credentials (for uploading secret)
-UPLOAD_USER_ALIAS="uploader"
-UPLOAD_USERNAME="uploader-user"
-UPLOAD_ACCESS_KEY="uploader-access-key"
-UPLOAD_SECRET_KEY="uploader-secret-key"
+UPLOAD_USER_ALIAS="mister07"
+UPLOAD_USERNAME="mister07-usr"
+UPLOAD_ACCESS_KEY="tbd"
+UPLOAD_SECRET_KEY="tbd"
 
 # Common values
 POLICY_NAME="upload-policy"
@@ -21,32 +21,30 @@ BUCKET_NAME="test-bucket"
 FILE_TO_UPLOAD="./secret.txt"
 OBJECT_NAME="secret.txt"
 
-deploy_challenge() {
-    docker compose up -d
+
+function create_minio_user() {
+    mc alias set "$ADMIN_ALIAS" "http://$ADMIN_ENDPOINT" "$ADMIN_ACCESS_KEY" "$ADMIN_SECRET_KEY"
+    mc admin user add "$ADMIN_ALIAS" "$ add" "$UPLOAD_SECRET_KEY"
 }
 
-create_minio_user_and_policy() {
-    mc alias set "$ADMIN_ALIAS" "http://$ADMIN_ENDPOINT" "$ADMIN_ACCESS_KEY" "$ADMIN_SECRET_KEY"
-
-    # Create user
-    mc admin user add "$ADMIN_ALIAS" "$ add" "$UPLOAD_SECRET_KEY"
-
-    # Create and attach policy
+function create_and_attach_minio_policy() {
     mc admin policy create "$ADMIN_ALIAS" "$POLICY_NAME" "$POLICY_FILE_PATH"
     mc admin policy attach "$ADMIN_ALIAS" "$POLICY_NAME" --user="$UPLOAD_USERNAME"
 }
 
-put_secret() {
+
+function put_secret() {
     # Login as uploader user
     mc alias set "$UPLOAD_USER_ALIAS" "http://$ADMIN_ENDPOINT" "$UPLOAD_USERNAME" "$UPLOAD_SECRET_KEY"
 
-    # Create bucket and upload using the user (if bucket exists already, skip error)
-    mc mb "$UPLOAD_USER_ALIAS/$BUCKET_NAME" || echo "Bucket $BUCKET_NAME already exists"
     mc version enable "$UPLOAD_USER_ALIAS/$BUCKET_NAME"
     mc cp "$FILE_TO_UPLOAD" "$UPLOAD_USER_ALIAS/$BUCKET_NAME/$OBJECT_NAME"
 }
 
-# Execute
-deploy_challenge
-create_minio_user_and_policy
-put_secret
+function main() {
+    create_minio_user()
+    create_and_attach_minio_policy()
+    put_secret()
+}
+
+main
