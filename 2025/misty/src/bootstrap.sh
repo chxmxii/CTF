b20e32bfabde77ac95c0779ef3f2e4d608564c95
Mouhib Chamsi <94466413+chxmxii@users.noreply.github.com>
2025-05-03 17:54:50 +0000

    feat(misty): finalizied misty challenge

--- 2025/misty/src/bootstrap.sh
+++ 2025/misty/src/bootstrap.sh
@@ -2,29 +2,27 @@
 
 set -euo pipefail
 
-# admin creds
-ADMIN_ENDPOINT="localhost:9000"
-ADMIN_ACCESS_KEY="admin-access-key"
-ADMIN_SECRET_KEY="admin-secret-key"
+ENDPOINT="https://dev-minio.tekup-securinets.tech/"
+
+ADMIN_ACCESS_KEY="${ADMIN_ACCESS_KEY:?Environment variable ADMIN_ACCESS_KEY is required}"
+ADMIN_SECRET_KEY="${ADMIN_SECRET_KEY:?Environment variable ADMIN_SECRET_KEY is required}"
 ADMIN_ALIAS="admin"
 
-# User credentials (for uploading secret)
-UPLOAD_USER_ALIAS="mister07"
-UPLOAD_USERNAME="mister07-usr"
-UPLOAD_ACCESS_KEY="tbd"
-UPLOAD_SECRET_KEY="tbd"
+USER_ALIAS="tmp-user"
+USERNAME="chx-dev037"
+USER_ACCESS_KEY="${USER_ACCESS_KEY:?Environment variable UPLOAD_ACCESS_KEY is required}"
+USER_SECRET_KEY="${USER_SECRET_KEY:?Environment variable UPLOAD_SECRET_KEY is required}"
 
-# Common values
-POLICY_NAME="upload-policy"
+POLICY_NAME="TMP Policy"
 POLICY_FILE_PATH="./policy.json"
-BUCKET_NAME="test-bucket"
-FILE_TO_UPLOAD="./secret.txt"
-OBJECT_NAME="secret.txt"
-
+BUCKET_NAME="tmp-vault"
+FILE_TO_UPLOAD="./flag.txt"
+FILE_TO_UPLOAD_SECOND="./key"
+OBJECT_NAME="key"
 
 function create_minio_user() {
-    mc alias set "$ADMIN_ALIAS" "http://$ADMIN_ENDPOINT" "$ADMIN_ACCESS_KEY" "$ADMIN_SECRET_KEY"
-    mc admin user add "$ADMIN_ALIAS" "$ add" "$UPLOAD_SECRET_KEY"
+    mc alias set "$ADMIN_ALIAS" "$ADMIN_ENDPOINT" "$ADMIN_ACCESS_KEY" "$ADMIN_SECRET_KEY"
+    mc admin user add "$ADMIN_ALIAS" "$UPLOAD_USERNAME" "$UPLOAD_SECRET_KEY"
 }
 
 function create_and_attach_minio_policy() {
@@ -32,19 +30,18 @@ function create_and_attach_minio_policy() {
     mc admin policy attach "$ADMIN_ALIAS" "$POLICY_NAME" --user="$UPLOAD_USERNAME"
 }
 
-
 function put_secret() {
-    # Login as uploader user
     mc alias set "$UPLOAD_USER_ALIAS" "http://$ADMIN_ENDPOINT" "$UPLOAD_USERNAME" "$UPLOAD_SECRET_KEY"
 
     mc version enable "$UPLOAD_USER_ALIAS/$BUCKET_NAME"
     mc cp "$FILE_TO_UPLOAD" "$UPLOAD_USER_ALIAS/$BUCKET_NAME/$OBJECT_NAME"
+    mc cp "$FILE_TO_UPLOAD_SECOND" "$UPLOAD_USER_ALIAS/$BUCKET_NAME/$OBJECT_NAME"
 }
 
 function main() {
-    create_minio_user()
-    create_and_attach_minio_policy()
-    put_secret()
+    create_minio_user
+    create_and_attach_minio_policy
+    put_secret
 }
 
 main
